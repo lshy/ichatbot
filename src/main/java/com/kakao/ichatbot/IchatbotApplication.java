593124655e09314da85fed692bfdcf94c4b1b627
lshy <lshy_@naver.com>
2019-02-22 17:45:30 +0900

    Init Project

---
+++ src/main/java/com/kakao/ichatbot/IchatbotApplication.java
@@ -0,0 +1,14 @@
+package com.kakao.ichatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IchatbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IchatbotApplication.class, args);
+        System.out.println("Init Project");
+    }
+
+}
