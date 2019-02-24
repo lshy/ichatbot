466d32ee39012cb66424f14c4dfe9fd7d4a48bef
lshy <lshy_@naver.com>
2019-02-24 23:30:38 +0900

    add dependencies & hello string (#1)
    ok

--- src/main/java/com/kakao/ichatbot/IchatbotApplication.java
+++ src/main/java/com/kakao/ichatbot/IchatbotApplication.java
@@ -8,7 +8,7 @@ public class IchatbotApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(IchatbotApplication.class, args);
-        System.out.println("Init Project");
+        System.out.println("Start i Chatbot!");
     }
 
 }
