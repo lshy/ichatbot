01624ce048cd1bda1677e80cdf95c1afacc41af7
lshy <lshy_@naver.com>
2019-02-27 12:43:00 +0900

    TestController 변경

--- src/main/java/com/kakao/ichatbot/example/TestController.java
+++ src/main/java/com/kakao/ichatbot/example/TestController.java
@@ -17,54 +17,63 @@ import java.util.stream.Stream;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 
 @RestController
-@RequestMapping(value="/test")
+@RequestMapping(value = "/test")
 public class TestController {
-
 
     @PostMapping("/data")
     @ResponseStatus(value = HttpStatus.OK)
-    private ResponseEntity<SkillResponse> dataResponse(@RequestBody SkillPayload skillPayload){
+    private ResponseEntity<SkillResponse> dataResponse() {
 
         URI uri = linkTo(TestController.class).toUri();
         Tester tester = Tester.builder().name("asd").address("asdaaaa").build();
 
-        return ResponseEntity.created(uri).body(SkillResponse.builder().data(tester).build());
+        //body의 SkillResponse는 Service에서 구현 후 반환
+        SkillResponse skillResponse = SkillResponse
+                .builder()
+                .data(tester)
+                .build();
+
+        return ResponseEntity.created(uri).body(skillResponse);
 
     }
 
     @PostMapping("/template")
     @ResponseStatus(value = HttpStatus.OK)
-    private ResponseEntity<SkillResponse> templateResponse(@RequestBody SkillPayload skillPayload){
+    private ResponseEntity<SkillResponse> templateResponse() {
 
         URI uri = linkTo(TestController.class).toUri();
         List<ISkillComponent> outputs = Stream.of(
                 SimpleText.builder().text("hello my name is Yoongi!").build()
         ).collect(Collectors.toList());
 
-        return ResponseEntity.created(uri).body(
-                SkillResponse.builder()
-                        .template(SkillTemplate.builder()
-                                .outputs(outputs)
-                                .build())
-                        .build());
+        //body의 SkillResponse는 Service에서 구현 후 반환
+        SkillResponse skillResponse = SkillResponse.builder()
+                .template(SkillTemplate.builder()
+                        .outputs(outputs)
+                        .build()).build();
+
+        return ResponseEntity.created(uri).body(skillResponse);
 
     }
 
     @PostMapping("/payload")
     @ResponseStatus(value = HttpStatus.OK)
-    private ResponseEntity<SkillResponse> payloadResponse(@RequestBody SkillPayload skillPayload){
+    private ResponseEntity<SkillResponse> payloadResponse(@RequestBody SkillPayload skillPayload) {
 
         URI uri = linkTo(TestController.class).toUri();
         List<ISkillComponent> outputs = Stream.of(
                 SimpleText.builder().text(skillPayload.getAction().getName()).build()
         ).collect(Collectors.toList());
 
-        return ResponseEntity.created(uri).body(
-                SkillResponse.builder()
-                        .template(SkillTemplate.builder()
-                                .outputs(outputs)
-                                .build())
-                        .build());
+        //body의 SkillResponse는 Service에서 구현 후 반환
+        SkillResponse skillResponse = SkillResponse
+                .builder()
+                .template(SkillTemplate
+                        .builder()
+                        .outputs(outputs)
+                        .build()).build();
+
+        return ResponseEntity.created(uri).body(skillResponse);
 
     }
 }
